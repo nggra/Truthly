dbe269057dee6aed3dddb3035fbbd61a637b53e1
ICP.Ninja <noreply@icp.ninja>
2025-03-29 09:17:20 +0700

    ICP.Ninja export

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+      extend: {
+        colors: {
+          purpleWhite: "#F5F3FF",
+          lightBlue: "#60A5FA",
+        },
+      },
+    },
+    plugins: [],
+};
